d257e1f2bcbda564898c3fb4c574a850d6127539
DiegoPinto357 <diego.pinto357@gmail.com>
2021-08-08 23:31:52 -0300

    Resolve captcha using 2captcha service for gov.br

--- src/libs/govbr.js
+++ src/libs/govbr.js
@@ -1,8 +1,11 @@
 const axios = require('axios');
+const poll = require('promise-poller').default;
 const { sites, services } = require('../../data');
 
 const googlekey = '6LeVYrUUAAAAAM7ZsAaZS5tpZOD6AwG8-1Nm6Iep';
 const pageurl = 'https://sso.acesso.gov.br/login';
+
+const timeout = millis => new Promise(resolve => setTimeout(resolve, millis));
 
 const getCaptchaRequestId = async apiKey => {
   const params = {
@@ -20,9 +23,50 @@ const getCaptchaRequestId = async apiKey => {
   return response.data.request;
 };
 
+const requestCaptchaResult = (apiKey, requestId) => {
+  return async function () {
+    return new Promise(async function (resolve, reject) {
+      const params = {
+        key: apiKey,
+        action: 'get',
+        id: requestId,
+        json: 1,
+      };
+
+      const response = await axios.get('http://2captcha.com/res.php', {
+        params,
+      });
+
+      if (response.data.status === 0) {
+        const { request } = response.data;
+        console.log(request);
+        return reject(request);
+      }
+      resolve(response.data.request);
+    });
+  };
+};
+
+const pollForCaptchaResult = async (
+  key,
+  id,
+  retries = 30,
+  interval = 1500,
+  delay = 15000
+) => {
+  await timeout(delay);
+  return poll({
+    taskFn: requestCaptchaResult(key, id),
+    interval,
+    retries,
+  });
+};
+
 const login = async page => {
-  // const requestId = await getCaptchaRequestId(services['2captcha'].apiKey);
-  // console.log({ requestId });
+  const { apiKey } = services['2captcha'];
+
+  const requestId = await getCaptchaRequestId(apiKey);
+  console.log({ requestId });
 
   await page.waitForSelector('#accountId');
   await page.type('#accountId', sites['gov.br'].username);
@@ -30,7 +74,15 @@ const login = async page => {
 
   await page.waitForSelector('#password');
   await page.type('#password', sites['gov.br'].password);
-  await page.click('.button-ok', { delay: 5000 });
+
+  const captchaResponse = await pollForCaptchaResult(apiKey, requestId);
+  console.log({ captchaResponse });
+
+  await page.evaluate(
+    `document.querySelector('[title="desafio reCAPTCHA"]').contentWindow.document.getElementById("recaptcha-token").innerHTML="${captchaResponse}";`
+  );
+
+  await page.click('.button-ok');
 };
 
 module.exports = {
